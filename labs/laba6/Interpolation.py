5b16f4eeff3afcf9b7c32802f693cfd126968e0d
Garapace <starkov-2003@list.ru>
2024-05-26 15:22:57 +0300

    поправил лабы 6-7

--- labs/laba6/Interpolation.py
+++ labs/laba6/Interpolation.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 1. https://ru.wikipedia.org/wiki/Интерполяционный_многочлен_Лагранжа
 2. https://www.youtube.com/watch?v=nj2RBZ6xFeY
 3. https://goo.su/4skJFSh --> глава 1, страница 42
-=============== Начальные условия ===============
+=============== Начальные условия ================
               y = |x|, x ∈ [-2; 2]
 """
 
@@ -19,30 +19,35 @@ def method_lagrange(x_values, y_values, x):
     :return:          - результат вычислений методом Лагранжа
     """
     result = 0
-    for i in range(len(x_values)): # реализация метода Лагранжа
-        step = y_values[i]
+    for i in range(len(x_values)):  # реализация метода Лагранжа
+        li_x = y_values[i]
         for j in range(len(x_values)):
             if i != j:
-                step *= (x - x_values[j]) / (x_values[i] - x_values[j])
-        result += step
+                li_x *= (x - x_values[j]) / (x_values[i] - x_values[j])
+        result += li_x
     return result
 
 
 def main():
     x = 3      # x для примера
-    steps = 6  # кол-во точек для графика
-    x_values = np.linspace(-2, 2, steps)  # значения функции по оси ординат
-    y_values = abs(x_values)                    # значения функции по оси абсцисс
+    steps = 7  # кол-во точек для графика
+    a = 2
+    b = -2
+
+    x_values = np.linspace(-2, 2, steps)
+    ch_values = [(b + a) / 2 + ((b - a) / 2) * np.cos(((2 * i + 1) / (2 * (steps + 1))) * np.pi)
+                 for i in range(steps)]  # значения функции по оси ординат
+    y_values = np.abs(ch_values)              # значения функции по оси абсцисс
 
     print(f"x = {x_values}\ny = {y_values}")
     print(f"при x = {x} y = {np.round(method_lagrange(x_values, y_values, x), 2)}")
 
-    x_i_values = np.linspace(-2, 2, 101)
-    y_i_values = method_lagrange(x_values, y_values, x_i_values)
+    x_i_values = np.linspace(-2, 2, 200)
+    y_i_values = method_lagrange(ch_values, y_values, x_i_values)
 
-    plt.plot(x_i_values, np.abs(x_i_values), label="y = |x|")           # вывод оригинального графика
-    plt.plot(x_i_values, y_i_values, label="Интерполяция Лагранжа")     # график через метод Лагранжа
-    plt.scatter(x_values, y_values, color="black", label="Начальные данные")  # точки для метода Лагранжа
+    plt.plot(x_values, np.abs(x_values), label="y = |x|")                 # вывод оригинального графика
+    plt.plot(x_i_values, y_i_values, label="Интерполяция Лагранжа")           # график через метод Лагранжа
+    plt.scatter(ch_values, y_values, color="black", label="Начальные данные")  # точки для метода Лагранжа
     plt.xlabel("x")
     plt.ylabel("y")
     plt.legend()
--- labs/laba7/Sorts.py
+++ labs/laba7/Sorts.py
@@ -1,17 +1,29 @@
+import random
+from time import time
+
+
 """  Сортировки: пузырьковая, быстрая, подсчётом  """
+
+count_bubble = 0
+count_quick = 0
+count_count = 0
 
 
 def bubble_sort(array):
+    global count_bubble
     for i in range(len(array)):
         for j in range(len(array)):
             if array[i] < array[j]:
                 temp = array[i]
                 array[i] = array[j]
                 array[j] = temp
+                count_bubble += 1
     return array
 
 
 def quick_sort(array):
+    global count_quick
+    count_quick += 1
     if len(array) > 1:
         pivot = array[len(array) // 2]             # определяем опорный элемент,
         less = [u for u in array if u < pivot]     # массив с элементами меньше опорного
@@ -23,24 +35,44 @@ def quick_sort(array):
 
 
 def count_sort(array):
+    global count_count
     count = [0] * (max(array) - min(array) + 1)  # массив подсчёта встречающихся элементов
 
     for num in array:                            # подсчёт встречающихся элементов
         count[num - min(array)] += 1
+        count_count += 1
 
     restored_array = []                          # восстанавливаем массив с помощью досчитанных значений
     for i, num in enumerate(count):
         restored_array.extend([i + min(array)] * num)
-    return  restored_array
+        count_count += num
+    return restored_array
 
 
 def main():
-    array = [7, 1, 15, 90, 5, 3, 71, 23, 15, 40] # начальный массив для сортировки
+    # array = [7, 1, 15, 90, 5, 3, 71, 23, 15, 40] # начальный массив для сортировки
+    array = []
+    for i in range(2000):
+        array.append(random.randint(-1000, 1000))
+    print(f"неотсортированный массив:\t{array}\n")
 
-    print(f"неотсортированный массив:\t{array}")
+    start_bubble = time()
     print(f"пузырьковая сортировка:\t\t{bubble_sort(array)}")
+    end_bubble = time()
+    print(f"время бабл {end_bubble - start_bubble}")
+    print(f"итерации бабл - {count_bubble}\n")
+
+    start_quick = time()
     print(f"быстрая сортировка:\t\t\t{quick_sort(array)}")
+    end_quick = time()
+    print(f"время квик {end_quick - start_quick}")
+    print(f"итерации квик - {count_quick}\n")
+
+    start_count = time()
     print(f"сортировка суммированием:\t{count_sort(array)}")
+    end_count = time()
+    print(f"время подсчёт {end_count - start_count}")
+    print(f"итерации подсчёт - {count_count}\n")
 
 
 if __name__ == "__main__":
